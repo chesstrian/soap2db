6727d0aa802562195b2f6f4297cde495c8adcae9
Christian Gutierrez <chesstrian@gmail.com>
2016-10-03 18:51:16 -0500

    Complete get library

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+var moment = require("moment");
+
 var lib = require("./lib/index");
 
 lib.getToken(function (err, token) {
-  console.log(err || token);
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(token);
+
+  lib.getLeagues(token, function (err, leagues) {
+    console.log(leagues);
+  });
+
+  lib.getEvents(token, function (err, events) {
+    console.log(events);
+  });
+
+  lib.getScores(token, function (err, scores) {
+    console.log(scores);
+  });
+
+  lib.getSportsbooks(token, function (err, sportbooks) {
+    console.log(sportbooks);
+  });
+
+  lib.getOdds(token, null, null, moment(new Date()).format(), function (err, odds) {
+    console.log(odds);
+  });
 });
