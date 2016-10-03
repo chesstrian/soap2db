d7a425af65b860737742632c0880ee64b9ce2a7a
Christian Gutierrez <chesstrian@gmail.com>
2016-10-03 17:29:16 -0500

    Add a project structure for methods

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-var soap = require("soap");
+var lib = require("./lib/index");
 
-var url = "http://74.208.226.147/FeedServiceBR/SMTO.Feed.Service.FeedService.svc?singleWsdl";
-
-soap.createClient(url, function (err, client) {
-  var args = {
-    username: "GoldenTest",
-    password: "123456"
-  };
-
-  client.GenerateToken(args, function (err, result) {
-    console.log(result);
-  });
+lib.getToken(function (err, token) {
+  console.log(err || token);
 });
