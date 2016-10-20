a09d16643c1f77880b37f9fdd03bcc80c09a1a51
Christian Gutierrez <chesstrian@gmail.com>
2016-10-20 17:28:31 -0500

    Scripts and README file

---
+++ README.md
@@ -0,0 +1,29 @@
+# Soap2DB
+
+Service to get data from soap service and save into a SQL Server Database
+
+## Install
+
+```bash
+npm install
+npm install -g forever
+
+cp soap2db.sh /etc/init.d/soap2db
+cp soap2db-gui.sh /etc/init.d/soap2db-gui
+
+chmod +x /etc/init.d/soap2db /etc/init.d/soap2db-gui
+
+chkconfig --add soap2db
+chkconfig --add soap2db-gui
+```
+
+## Run
+
+```bash
+service soap2db-gui start
+service soap2db start
+```
+
+## Access
+
+Go to http://server:3000/ in a browser, check `web/routes.js` at line 7.
---
+++ soap2db-gui.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Note runlevel 2345, 86 is the Start order and 85 is the Stop order
+#
+# chkconfig: 2345 86 85
+# description: Description of the Service
+#
+# Below is the source function library, leave it be
+. /etc/init.d/functions
+
+# result of whereis forever or whereis node
+# export PATH=$PATH:/usr/local/bin
+# result of whereis node_modules
+# export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
+
+
+start(){
+    DEBUG=app:* forever start /home/user/soap2db/app.js
+}
+
+stop(){
+    forever stop /home/user/soap2db/app.js
+}
+
+restart(){
+    forever restart /home/user/soap2db/app.js
+}
+
+case "$1" in
+  start)
+    echo "Start service soap2db web"
+    start
+    ;;
+  stop)
+    echo "Stop service soap2db web"
+    stop
+    ;;
+  restart)
+    echo "Restart service soap2db web"
+    restart
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+    ;;
+esac
